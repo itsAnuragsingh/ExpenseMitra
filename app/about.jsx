b4648882ef13dbe369a74d66d6f9d71e7930201e
Anurag Singh <itsanurag707@gmail.com>
2025-06-27 22:23:07 +0530

    setup auth and signin and signup screen

---
+++ app/about.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class about extends Component {
+  render() {
+    return (
+      <View>
+        <Text>about</Text>
+      </View>
+    )
+  }
+}
